388e734b46379e9a07902dcb5f0b4e573e1dcd60
nithishkaviyan <34723188+nithishkaviyan@users.noreply.github.com>
2019-01-20 23:41:22 -0600

    Update Yelp Selecting Review Data for Sentiment Analysis.py

--- Yelp Selecting Review Data for Sentiment Analysis.py
+++ Yelp Selecting Review Data for Sentiment Analysis.py
@@ -1,21 +1,9 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[2]:
-
 
 import json
 import pandas as pd
 
-
-# In[3]:
-
-
 #Path for json file
 path=open("yelp_academic_dataset_review.json",'r',encoding='utf8')
-
-
-# In[4]:
 
 
 ##Read json file
@@ -24,134 +12,51 @@ for i,x in enumerate(path):
     data_list.append(json.loads(x))
 
 
-# In[5]:
-
-
-print(len(data_list))
-
-
-# In[ ]:
-
-
-
-
-
-# In[6]:
-
-
 ##Convert list to pandas DataFrame
 data_reviews=pd.DataFrame.from_dict(data_list,orient='columns')
-
-
-# In[7]:
-
-
-print(data_reviews.stars.value_counts())
-
-
-# In[8]:
 
 
 ##Read selected users file
 user_data=pd.read_csv('select_users.csv',sep=',')
 
-
-# In[9]:
-
-
 user_id=user_data['user_id']
-
-
-# In[10]:
 
 
 select_review=data_reviews[data_reviews['user_id'].isin(user_id)].reset_index(drop=True)
 
-
-# In[11]:
-
-
 select_review=select_review[['user_id','review_id','business_id','text','stars']]
-
-
-# In[ ]:
-
-
-
-
-
-# In[12]:
 
 
 ##Read selected users_neg file
 user_data_neg=pd.read_csv('select_users_neg.csv',sep=',')
 
-
-# In[13]:
-
-
 user_id_neg=user_data_neg['user_id']
-
-
-# In[14]:
 
 
 select_reviews_12=data_reviews[(data_reviews.stars==1) | (data_reviews.stars==2)].reset_index(drop=True)
 
 
-# In[15]:
-
-
 user_id_neg=user_data_neg[user_data_neg.review_count > 310]['user_id'].reset_index(drop=True)
-
-
-# In[16]:
 
 
 select_reviews_12=select_reviews_12[select_reviews_12['user_id'].isin(user_id_neg)]
 select_reviews_12=select_reviews_12[['user_id','review_id','business_id','text','stars']]
 
 
-# In[17]:
-
-
 select_reviews=pd.concat([select_review,select_reviews_12],ignore_index=True)
-
-
-# In[ ]:
-
-
-
-
-
-# In[18]:
 
 
 ##Count of number of stars in sample reviews
 print(select_reviews.stars.value_counts())
 
 
-# In[19]:
-
-
 ##Drop reviews with stars 3 since its neither a positive nor a negative review
 select_reviews=select_reviews[~(select_reviews.stars==3)]
 
 
-# In[ ]:
-
-
-
-
-
-# In[20]:
-
 
 ##Write select_reviews to a csv file
 select_reviews.to_csv('select_reviews.csv')
-
-
-# In[21]:
 
 
 ##Clean reviews and store in a text file
